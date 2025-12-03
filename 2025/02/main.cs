91dc178d417c1659bf4ff8fefd5bcba32fe73f84
Miguel Borges <up202004481@up.pt>
2025-12-03 12:56:00 +0000

    push past two days solutions

---
+++ 2025/02/main.cs
@@ -0,0 +1,58 @@
+﻿var input = System.IO.File.ReadAllText("input.txt");
+
+bool IsSilverValid(ulong n) {
+	int l = (int) Math.Floor(Math.Log10(n) + 1);
+	if (l % 2 != 0) return false;
+	ulong d = (ulong)Math.Pow(10, l / 2);
+	return n % d == n / d;
+}
+
+bool IsGoldValid(ulong n) {
+	int l = (int) Math.Floor(Math.Log10(n) + 1);
+	int hl = l / 2;
+	for (int sl = 1; sl <= hl; sl += 1) {
+		var nsl = l / sl;
+		var nslrem = l % sl;
+		if (nslrem != 0 || nsl < 2) {
+			continue;
+		}
+		var bten = (ulong) Math.Pow(10, sl);
+		var to_divide = n / bten;
+		var target = n % bten;
+		bool valid = true;
+		while (to_divide > 0) {
+			var curr_rem = to_divide % bten;
+			var next_to_divide = to_divide / bten;
+			if (curr_rem != target) {
+				valid = false;
+				break;
+			} else {
+				to_divide = next_to_divide;
+			}
+		}
+		if (valid) {
+			return true;
+		}
+	}
+	return false;
+}
+
+var commands = input.Split(',');
+var ranges = commands.ToList().ConvertAll(c => c.Split('-').ToList().ConvertAll(s => ulong.Parse(s)));
+
+ulong silver = 0;
+ulong gold = 0;
+
+foreach (var r in ranges) {
+	for (ulong n = r[0]; n <= r[1]; n += 1) {
+		if (IsSilverValid(n)) {
+			silver += n;
+		}
+		if (IsGoldValid(n)) {
+			gold += n;
+		}
+	}
+}
+
+Console.WriteLine($"Silver: {silver}");
+Console.WriteLine($"Gold: {gold}");
---
+++ 2025/03/main.cs
@@ -0,0 +1,49 @@
+﻿int silver = 0;
+long gold = 0;
+
+
+using (var reader = new System.IO.StreamReader("input.txt")) {
+	string? line;
+	while ((line = reader.ReadLine()) != null) {
+		// Silver
+		int decimals_i = 0;
+		char decimals_v = (char) 0;
+		for (int i = 0; i < line.Length - 1; i += 1) {
+			if (line[i] > decimals_v) {
+				decimals_i = i;
+				decimals_v = line[i];
+				if (decimals_v == '9') break;
+			}
+		}
+		char units_v = (char) 0;
+		for (int i = decimals_i + 1; i < line.Length; i += 1) {
+			if (line[i] > units_v) {
+				units_v = line[i];
+				if (units_v == '9') break;
+			}
+		}
+		silver += (decimals_v - '0') * 10 + (units_v - '0');
+
+
+		// Gold
+		int idx = 11;
+		int last_written_idx = -1;
+		long n_to_sum = 0;
+		while (idx >= 0) {
+			char current_max = (char) 0;
+			for (int i = last_written_idx + 1; i < line.Length - idx; i += 1) {
+				if (line[i] > current_max) {
+					current_max = line[i];
+					last_written_idx = i;
+					if (line[i] == '9') break;
+				}
+			}
+			idx -= 1;
+			n_to_sum = n_to_sum * 10 + (long) (current_max - '0');
+		}
+		gold += n_to_sum;
+	}
+}
+
+Console.WriteLine($"Silver: {silver}");
+Console.WriteLine($"Gold: {gold}");
